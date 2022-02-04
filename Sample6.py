5f9413de6ac51b6cfc5fd68033663ac7f0f59f1a
Y-Latha <98838686+Y-Latha@users.noreply.github.com>
2022-02-04 21:36:55 +0000

    Add files via upload
    Made changes according to the question.

--- Sample6.py
+++ Sample6.py
@@ -1,15 +1,18 @@
+# Program than takes two integers from user to perform mathematical operation according to the user's choice 
 First_Num = int(input("Please enter a number between 1 and 100: "))
 Second_Num = int(input("Please enter another number between 1 and 100: "))
-print("\n 1> If you want to add the numbers type 1  \n 2> If you want the difference between the numbers type 2 \n 3> If you want product of the numbers type 3 \n 4> If you want to divide first with second number type 4 ")
-User_Choice= int(input("\n Please enter the number according to the menu above : "))
-if User_Choice == 1:
+print("\n 1> If you want to add the numbers type a \n 2> If you want the difference between the numbers type b \n 3> If you want product of the numbers type c \n 4> If you want to divide first with second number type d \n 5> If you want to get the square of numbers type e ")
+User_Choice= input("\n Please enter the letter according to the menu above : ")
+if User_Choice == "a":
      print(First_Num + Second_Num )
-elif  User_Choice == 2:
+elif  User_Choice == "b":
      if First_Num > Second_Num:
       print(First_Num - Second_Num )
      else:
       print(Second_Num - First_Num )   
-elif User_Choice == 3:
+elif User_Choice == "c":
      print(First_Num * Second_Num )
+elif User_Choice == "d":
+     print(First_Num / Second_Num )    
 else :
-     print(First_Num / Second_Num )
+     print(First_Num ** Second_Num )
