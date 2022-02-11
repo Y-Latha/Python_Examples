ace37cc4131a52683fc0697c61743bce84d11ba1
Y-Latha <98838686+Y-Latha@users.noreply.github.com>
2022-02-11 10:06:19 +0000

    Add files via upload

---
+++ Example 7.py
@@ -0,0 +1,6 @@
+# TASK1:Write a program that asks the user for an integer number and checks if it is > 10. If it is, it will print “Number is Greater than 10”, else “Number is smaller than 10”.
+User_Num = int(input("Please enter an interger : "))
+if User_Num > 10:
+    print("Number is greater than 10") 
+else:
+    print("Number is smaller than 10")
